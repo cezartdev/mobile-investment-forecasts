633b20e6a3b0af7139d4ad1198a085a68e55b4a4
cezartdev <cesaralonsosalazarp@gmail.com>
2024-10-06 17:25:09 -0300

    I've made a Sidebar component

--- src/pages/HomeScreen.tsx
+++ src/pages/HomeScreen.tsx
@@ -1,16 +1,25 @@
-// screens/HomeScreen.tsx
-import React from 'react';
-import { View, Text, Button } from 'react-native';
+// src/pages/HomeScreen.tsx
+import React, { useState } from 'react';
+import { SafeAreaView, View, Text } from 'react-native';
+import Sidebar from '../components/Sidebar';
+import { RootStackParamList } from '../types/navigation';
 
-const HomeScreen = ({ navigation }: { navigation: any }) => {
+const HomeScreen = () => {
+  const [selectedOption, setSelectedOption] = useState<string>('');
+
+  // Define las opciones como un arreglo de objetos
+  const options: { label: string; route: keyof RootStackParamList }[] = [
+    { label: 'Opción 1', route: 'Home' },
+    { label: 'Opción 2', route: 'Details' },
+    // Puedes añadir más opciones aquí
+  ];
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Inicio</Text>
-      <Button
-        title="Ir a Detalles"
-        onPress={() => navigation.navigate('Details')}
-      />
-    </View>
+    <SafeAreaView style={{ flex: 1, flexDirection: 'row' }}>
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Opción seleccionada: {selectedOption}</Text>
+      </View>
+    </SafeAreaView>
   );
 };
 
---
+++ src/types/navigation.ts
@@ -0,0 +1,7 @@
+// src/types/navigation.ts
+export type RootStackParamList = {
+    Home: undefined; // No se pasa ningún parámetro
+    Details: undefined; // No se pasa ningún parámetro
+    
+    // Agrega más rutas aquí según sea necesario
+  };
